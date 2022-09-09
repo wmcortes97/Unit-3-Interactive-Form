f8ad55a6d6b92bc68e41adebed517db353752c23
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-09 01:21:30 -0700

    hiding other job element

---
+++ interactive-form-v3/js/script.js
@@ -0,0 +1,12 @@
+//add focus state to the name field
+
+document.getElementById("name").focus();
+
+//initially hide other job role text field
+let otherJob = document.getElementById("other-job-role");
+otherJob.style.display = "none";
+//disply other-job-role when 'other' is selected
+
+const jobTitle = document.getElementById("title");
+
+
