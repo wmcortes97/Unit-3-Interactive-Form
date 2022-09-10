42753695d65f7cd112efd53c86c4c4b56b13cc2b
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-09 19:16:02 -0700

    Complete t shirt option

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -27,10 +27,30 @@ jobTitle.addEventListener("change", (e) => {
 /**
  * T-SHIRT INFO
  * disable the color option
+ * group color options based on data theme
  */
 const chooseColor = document.getElementById("color");
-//chooseColor.disabled = true;
+chooseColor.disabled = true;
+
+const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
+const jsHearts = document.querySelectorAll('[data-theme="heart js"]');
 
 const chooseTheme = document.getElementById("design");
 
-const punsOnly = chooseTheme.addEventListener("change", (e) => {});
+chooseTheme.addEventListener("change", (e) => {
+  if (e.target.value === "js puns") {
+    chooseColor.disabled = false;
+    for (let i = 0; i < jsHearts.length; i++) {
+      jsHearts[i].style.display = "none";
+      jsPuns[i].removeAttribute("style");
+    }
+  } else if (e.target.value === "heart js") {
+    chooseColor.disabled = false;
+    for (let i = 0; i < jsPuns.length; i++) {
+      jsPuns[i].style.display = "none";
+      jsHearts[i].removeAttribute("style");
+    }
+  } else {
+    chooseColor.disabled = true;
+  }
+});
