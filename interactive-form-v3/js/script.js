94dc6c75336753d4bb595216a7aa3212c7674b1b
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-09 18:23:44 -0700

    Fixed other-job display

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,12 +1,36 @@
 //add focus state to the name field
-
+/**
+ * NAME FIELD
+ * set focus on name element when user opens page
+ */
 document.getElementById("name").focus();
 
-//initially hide other job role text field
+/**
+ * JOB ROLE
+ * hide other-jon-role input field
+ * display other-job-role when user selects 'other'
+ */
+
 let otherJob = document.getElementById("other-job-role");
 otherJob.style.display = "none";
-//disply other-job-role when 'other' is selected
 
 const jobTitle = document.getElementById("title");
 
+jobTitle.addEventListener("change", (e) => {
+  if (e.target.value === "other") {
+    otherJob.removeAttribute("style");
+  } else {
+    otherJob.style.display = "none";
+  }
+});
 
+/**
+ * T-SHIRT INFO
+ * disable the color option
+ */
+const chooseColor = document.getElementById("color");
+//chooseColor.disabled = true;
+
+const chooseTheme = document.getElementById("design");
+
+const punsOnly = chooseTheme.addEventListener("change", (e) => {});
